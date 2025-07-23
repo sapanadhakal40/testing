89743e5b8f5f41c477076cc0b2eac127179350da
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-07-24 00:14:43 +0300

    Add test for getTopMovies helper function: filter high rate movies ≥4.5

--- utils/helpers.test.ts
+++ utils/helpers.test.ts
@@ -1,3 +1,24 @@
+import { getTopMovies } from "./helpers";
+
 describe('getTopMovies', () => {
+const allmovies = [
+	{ title: "Titanic", rating: 4.5, imageUrl: "https://via.placeholder.com/150" },
+	{ title: "Shutter Island", rating: 4.0, imageUrl: "https://via.placeholder.com/150" },
+	{ title: "Parasite", rating: 5.0, imageUrl: "https://via.placeholder.com/150" },
+	{ title: "Inception", rating: 4.8, imageUrl: "https://via.placeholder.com/150" },
+	{ title: "The Dark Knight", rating: 4.9, imageUrl: "https://via.placeholder.com/150" },
+];
+
+
   it('should return movies with a rating of 4.5 or higher', () => {
-    const
+  const topMovies = getTopMovies(allmovies);
+		expect(topMovies).toEqual([
+			{ title: "Titanic", rating: 4.5, imageUrl: "https://via.placeholder.com/150" },
+			{ title: "Parasite", rating: 5.0, imageUrl: "https://via.placeholder.com/150" },
+			{ title: "Inception", rating: 4.8, imageUrl: "https://via.placeholder.com/150" },
+			{ title: "The Dark Knight", rating: 4.9, imageUrl: "https://via.placeholder.com/150" },
+		]);
+    expect(topMovies.length).toBe(4);
+	});
+
+});
