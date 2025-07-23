e01d8894ad1aca67d8368533aa4c522ad1a59e85
sapanadhakal40 <sapna.dhakal40@gmail.com>
2025-07-23 16:40:38 +0300

    config external image domain and fix hydration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["c8.alamy.com", "images.plex.tv", "musicart.xboxlive.com"],
+  },
 };
 
 export default nextConfig;
